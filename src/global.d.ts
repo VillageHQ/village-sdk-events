f275a8d7bab7f8f88e8ed43a1f3251aeaf98f2e6
Daniel Neto <me@danielneto.com>
2025-04-18 15:59:25 -0300

    Extends Village initialization options
    Adds support for customizable call-to-action paths in Village initialization through a new `PathCTA` interface. Updates the `init` method to accept a public key and optional configuration.
    
    Improves flexibility and customization for Village integration.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,15 +1,24 @@
 // src/globals.d.ts
 
-export { }; 
+export { };
 
 declare global {
+    interface PathCTA {
+        label: string;
+        callback: () => void;
+        style?: React.CSSProperties;
+    }
+
+    interface VillageInitOptions {
+        paths_cta?: PathCTA[];
+    }
     interface Window {
         Village?: {
             on?: (event: string, callback: (data: any) => void) => void;
             emit?: (event: string, data: any) => void;
             broadcast?: (event: string, data: any) => void;
             authorize?: (...args: any[]) => void;
-            init?: (...args: any[]) => void;
+            init?: (publicKey: string, options?: VillageInitOptions) => void;
             loaded?: boolean;
             q?: any[];
         };
